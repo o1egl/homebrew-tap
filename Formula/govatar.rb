05b0f9f6af2b601524daca58855ad9a3e431a4e7
Oleg Lobanov <oleg@lobanov.me>
2021-03-14 13:25:53 +0100

    Brew formula update for govatar version v0.4.1

--- Formula/govatar.rb
+++ Formula/govatar.rb
@@ -5,29 +5,29 @@
 class Govatar < Formula
   desc "GOvatar is an avatar generation library written in GO"
   homepage "https://github.com/o1egl/govatar"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/o1egl/govatar/releases/download/v0.4.0/darwin-amd64-govatar.tar.gz"
-    sha256 "309f22bec7edef2ef604eb05f6d841af7b51719e73a6b8a954a7ee178442855b"
+    url "https://github.com/o1egl/govatar/releases/download/v0.4.1/darwin-amd64-govatar.tar.gz"
+    sha256 "cd3168a3009bf0ccf1c6fbc1fb899884c08c1c23902f83c624520015f937f767"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/o1egl/govatar/releases/download/v0.4.0/darwin-arm64-govatar.tar.gz"
-    sha256 "742e6d100f474c32b8d8c3e852565fef34ff4ad1599e2006471c7d465f3be2fc"
+    url "https://github.com/o1egl/govatar/releases/download/v0.4.1/darwin-arm64-govatar.tar.gz"
+    sha256 "ad4bd8110b7d15ef3cc2e6c348fd54118d8d36801a0528a1c9a6559c25262f7f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/o1egl/govatar/releases/download/v0.4.0/linux-amd64-govatar.tar.gz"
-    sha256 "f829a8ff1622b65d2608ccd04bd1147df155bc0765577df8a8a8b1703f605009"
+    url "https://github.com/o1egl/govatar/releases/download/v0.4.1/linux-amd64-govatar.tar.gz"
+    sha256 "a286c4d61ff27cfbc08c85a388d9410171b170a856db059476347c3f0caca403"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/o1egl/govatar/releases/download/v0.4.0/linux-armv6-govatar.tar.gz"
-    sha256 "d0edaf4f0e46483f508a1ee9749e80c1177a8dc287970f81469cac70ddde6a38"
+    url "https://github.com/o1egl/govatar/releases/download/v0.4.1/linux-armv6-govatar.tar.gz"
+    sha256 "94d8e2b6f3cb6c452edf4751f55c2b3afc850e156e05a7c4c561bb7544d158e5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/o1egl/govatar/releases/download/v0.4.0/linux-arm64-govatar.tar.gz"
-    sha256 "1b92922d4f575fe831b2dce12a3132157930a2c41bccc42f8f242002a1753064"
+    url "https://github.com/o1egl/govatar/releases/download/v0.4.1/linux-arm64-govatar.tar.gz"
+    sha256 "4752c265343434376e751c7bdd49825007aa93d77f8db453e6544438096bb899"
   end
 
   def install
